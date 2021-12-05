dab1557e2cd142bdb2f3f434a2a087baa62395f4
rossmichaelyoung <rossmichaelyoung@gmail.com>
2021-12-05 13:46:13 -0600

    changed input parser

--- 2021/day5/day5.hs
+++ 2021/day5/day5.hs
@@ -7,7 +7,8 @@ import qualified Data.Map as M
 arrowParser :: Parser String
 arrowParser = Parser $ \input -> let (token, rest) = span (\x -> x == '-' || x == '>') input in Just (token, rest)
 
-parseInput input = runParser numPair input >>= \(x, input') -> runParser (wsParser *> arrowParser *> wsParser *> numPair) input' >>= \(x', input'') 
+inputParser :: Parser [(Int, Int)]
+inputParser = Parser $ \input -> runParser numPair input >>= \(x, input') -> runParser (wsParser *> arrowParser *> wsParser *> numPair) input' >>= \(x', input'') 
                     -> let x1 = head x; y1 = head $ tail x; x2 = head x'; y2 = head $ tail x' in Just ([(x1,y1), (x2, y2)], input'')
     where numPair = sepByParser commaParser naturalNumParser
 
@@ -32,7 +33,7 @@ addPointsToMap ventMap points = foldl' (\acc point -> M.adjust (+1) point acc) v
 
 main = do
     fileContent <- getArgAndReadFile
-    let ventLines = foldl' (\acc line -> let Just (x,_) = parseInput line in x : acc) [] (lines fileContent)
+    let ventLines = foldl' (\acc line -> let Just (x,_) = runParser inputParser line in x : acc) [] (lines fileContent)
     let ventMap = createVentMap ventLines
     let ventMap' = foldl' (\acc endPoints -> let points = findAllPointsBetweenPoints (head endPoints) (head $ tail endPoints) in addPointsToMap acc points) ventMap ventLines
     let sum = foldr (\val acc -> if val >= 2 then acc+1 else acc) 0 ventMap'
